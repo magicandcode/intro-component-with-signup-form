02997b28beee6eb7479167a922444c9208ad3ed8
magicandcode <magicandcode@gmail.com>
2019-07-07 00:22:22 +0200

    Add styles to error message class

--- js/validation.js
+++ js/validation.js
@@ -1,25 +1,43 @@
 const freeTrialForm = document.getElementById('free-trial-form');
 
 const freeTrialFormFields = document.querySelectorAll('#free-trial-form input');
+const freeTrialFormErrorMessages = document.querySelectorAll('.error-messge');
 
 const freeTrialSubmit = document.getElementById('free-trial-submit');
 
+freeTrialSubmit.addEventListener('click', e => {
+  e.preventDefault();
+  freeTrialFormFields.forEach(field => {
+    if (field.value === '') {
+      field.classList.add('validation-error');
+    } else {
+      field.classList.remove('validation-error');
+      //field.nextSibling.style.visibility = 'hidden';
+    }
+  });
+});
+/*
 freeTrialFormFields.forEach(field => {
-  
+  const index = Array.prototype.indexOf.call(freeTrialFormFields, field);
+  const errorMessage = freeTrialFormErrorMessages[index];
+/*
   if(field.value === '') {
       field.classList.add('validation-error');
+      errorMessage.getElementsByClassName.visibility = 'visible';
   }
-  
-  
+*/
+  /*
   field.addEventListener('focus', e => {
     if (field.value === '') {
       field.classList.toggle('validation-error');
+    }
   });
   field.addEventListener('onchange', e => {
     if (field.value === '') {
       field.classList.toggle('validation-error');
+    }
   });
-  
+  *
   freeTrialSubmit.addEventListener('click', e => {
     if (field.value === '') {
       e.preventDefault();
@@ -27,3 +45,4 @@ freeTrialFormFields.forEach(field => {
     }
   });
 });
+*/
