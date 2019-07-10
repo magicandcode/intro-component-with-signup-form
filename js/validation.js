821d8f4349c8a1a3418771d1f02170f68e65bcbc
magicandcode <magicandcode@gmail.com>
2019-07-10 20:12:20 +0200

    Refactor validation on form submit using object deconstruction

--- js/validation.js
+++ js/validation.js
@@ -1,129 +1,128 @@
 
-const freeTrialForm = document.getElementById('free-trial-form');
-const freeTrialFormFields = document.querySelectorAll('#free-trial-form input');
-const validationErrorClass = 'validation-error';
-const freeTrialFormErrorMessages = document.querySelectorAll('.error-messge');
-const freeTrialSubmitButton = document.getElementById('free-trial-submit');
+// Gets form fields to validate, in this case all fields are inputs and the
+// submit button is a button element; safe to select all form inputs
+const freeTrialFormFields = document.querySelectorAll(
+    '#free-trial-form input'
+);
 
+// Sets name of class to indicate an invalid input value
+// Class will be added to or removed from input indicating whether it is
+// invalid or not
+const validationErrorClass = 'validation-error';
+
+// Gets all error messages to display with error class
+// Error message will be either visible or hidden, depending on its input's
+// validity
+const freeTrialFormErrorMessages = document.querySelectorAll(
+    '.error-message'
+);
+
+// Gets form submit button
+const freeTrialSubmitButton = document.getElementById(
+    'free-trial-submit'
+);
+
+// Checks if form input type is email (requires specific validation rules)
 const  isEmailField = field => field.type === 'email';
 
-const isValidEmail = field => field.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i);
-const isValidField = field => field.value !== '';
+// Checks if form input value is valid email
+const isValidEmail = field => field.value.match(
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+);
 
-const setErrors = (field, errorMessage, event) => {
-    event.preventDefault();
+// Checks if form input value is not empty
+const isValidTextField = field => field.value !== '';
+
+// Adds error class to input and displays error message
+const addErrorIndicators = (field, errorMessage) => {
     field.classList.add(validationErrorClass);
     errorMessage.getElementsByClassName.visibility = 'visible';
 };
-const removeErrors = (field, errorMessage, event) => {
+
+// Removes error class from input and hides error message
+const removeErrorIndicators = (field, errorMessage) => {
     field.classList.remove(validationErrorClass);
     errorMessage.getElementsByClassName.visibility = 'hidden';
 };
 
-const validateField = (field, errorMessage, event) => {
+const isValidFormField = field => {
     if (isEmailField(field)) {
         if (!isValidEmail(field)) {
-            setErrors(field, errorMessage, event);
+            return false;
         } else {
-            removeErrors(field, errorMessage, event);
+            return true;
         }
     } else {
-        if (isValidField(field)) {
-            removeErrors(field, errorMessage, event);
+        if (isValidTextField(field)) {
+            return true;
         } else {
-            setErrors(field, errorMessage, event);
+            return false;
         }
     }
 };
-/*
-const validateFormOnSubmit = (fields, submitButton) => {
-    submitButton.addEventListener('click', e => {
+
+const validateFormField = (field, errorMessage) => {
+    if (isValidFormField(field)) {
+        removeErrorIndicators(field, errorMessage);
+    } else {
+        addErrorIndicators(field, errorMessage);
+    }
+};
+
+freeTrialFormFields.forEach(field => {
+    field.autocomplete = 'off'; // Turns off autocomplete from all form fields
+
+    // Gets index of current form field to get corresponding error message
+    const index = Array.prototype.indexOf.call(freeTrialFormFields, field);
+    const errorMessage = freeTrialFormErrorMessages[index];
+
+    // Adds validation on focus event
+    field.addEventListener(
+        'focus',
+        () => validateFormField(field, errorMessage)
+    );
+
+    // Adds validation on blur event
+    field.addEventListener(
+        'blur',
+        () => validateFormField(field, errorMessage)
+    );
+
+    // Adds validation on key up event
+    field.addEventListener(
+        'keyup',
+        () => validateFormField(field, errorMessage)
+    );
+});
+
+
+const validateFormOnSubmit = (form) => {
+
+    const {fields, errorMessages, submit} = form;
+
+    submit.addEventListener('click', e => {
         fields.forEach(field => {
-            if (isValidField(field)) {
-                if (!isValidEmail()) {
-                    e.preventDefault();
-                    validateField(field);
-                }
-            } else {
-                e.preventDefault();
-                validateField(field);
+            // Gets index of current form field to get corresponding error message
+            const index = Array.prototype.indexOf.call(fields, field);
+            const errorMessage = errorMessages[index];
+
+            // Adds or removes error indications
+            validateFormField(field, errorMessage);
+
+            // Prevents form to be submitted if any field is invalid
+            if (! isValidFormField(field)) {
+               e.preventDefault();
             }
         });
     });
 };
 
-//validateFormOnSubmit(freeTrialFormFields, freeTrialSubmitButton);
-*/
+const freeTrialForm = {
+    fields:  document.querySelectorAll('#free-trial-form input'),
+    errorClass: 'validation-error',
+    errorMessages: document.querySelectorAll('.error-message'),
+    submit: document.getElementById('free-trial-submit')
+};
 
-freeTrialFormFields.forEach(field => {
-    field.autocomplete = 'off';
-    const index = Array.prototype.indexOf.call(freeTrialFormFields, field);
-    const errorMessage = document.querySelectorAll('.error-message')[index];
-
-    field.addEventListener(
-        'focus',
-        e => validateField(field, errorMessage, e)
-    );
-
-    field.addEventListener(
-        'blur',
-        e => validateField(field, errorMessage, e)
-    );
-
-    field.addEventListener(
-        'keyup',
-        e => validateField(field, errorMessage, e)
-    );
-});
-freeTrialSubmitButton.addEventListener('click', e => {
-   freeTrialFormFields.forEach(field => {
-       const index = Array.prototype.indexOf.call(freeTrialFormFields, field);
-       const errorMessage = document.querySelectorAll('.error-message')[index];
-
-       validateField(field, errorMessage, e);
-   });
-});
-
-
-/*
-freeTrialSubmit.addEventListener('click', e => {
-  e.preventDefault();
-  freeTrialFormFields.forEach(field => {
-    if (field.value === '') {
-      field.classList.add('validation-error');
-    } else {
-      field.classList.remove('validation-error');
-      //field.nextSibling.style.visibility = 'hidden';
-    }
-  });
-});
-
-freeTrialFormFields.forEach(field => {
-  const index = Array.prototype.indexOf.call(freeTrialFormFields, field);
-  const errorMessage = freeTrialFormErrorMessages[index];
-
-  if(field.value === '') {
-      field.classList.add('validation-error');
-      errorMessage.getElementsByClassName.visibility = 'visible';
-  }
-
-
-  field.addEventListener('focus', e => {
-    if (field.value === '') {
-      field.classList.toggle('validation-error');
-    }
-  });
-  field.addEventListener('onchange', e => {
-    if (field.value === '') {
-      field.classList.toggle('validation-error');
-    }
-  });
-  *
-  freeTrialSubmit.addEventListener('click', e => {
-    if (field.value === '') {
-      e.preventDefault();
-      field.classList.add('validation-error');
-    }
-  });
-});
-*/
+validateFormFields(freeTrialForm); // Validates each field when interacted with
+validateFormOnSubmit(freeTrialForm); // Validates form before submitting
